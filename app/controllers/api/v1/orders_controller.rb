53394bec358fb1f45ae6998aaeba015d18b82001
axell-brendow <breno.axel@gmail.com>
2020-11-15 13:24:19 -0300

    Allows the order to be placed along with product quantity

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -14,7 +14,10 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def create
-    order = current_user.orders.build(order_params)
+    order = Order.create! user: current_user
+    order.build_placements_with_product_ids_and_quantities(
+      order_params[:product_ids_and_quantities]
+    )
 
     # if order.save
     order.save
@@ -28,7 +31,6 @@ class Api::V1::OrdersController < ApplicationController
   private
 
   def order_params
-    # params.require(:order).require(:product_ids)
-    params.require(:order).permit(:product_ids)
+    params.require(:order).permit(product_ids_and_quantities: %i[product_id quantity])
   end
 end
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -5,8 +5,10 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     @order = orders(:one)
     @order_params = {
       order: {
-        product_ids: [products(:one).id, products(:two).id],
-        total: 50
+        product_ids_and_quantities: [
+          { product_id: products(:one).id, quantity: 2 },
+          { product_id: products(:two).id, quantity: 3 }
+        ]
       }
     }
   end
@@ -49,11 +51,22 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :forbidden
   end
 
-  test 'should create order with two products' do
+  # test 'should create order with two products' do
+  #   assert_difference('Order.count', 1) do
+  #     post api_v1_orders_url, params: @order_params, headers: {
+  #       Authorization: JsonWebToken.encode(user_id: @order.user_id)
+  #     }, as: :json
+  #   end
+  #   assert_response :created
+  # end
+
+  test 'should create order with two products and placements' do
     assert_difference('Order.count', 1) do
-      post api_v1_orders_url, params: @order_params, headers: {
-        Authorization: JsonWebToken.encode(user_id: @order.user_id)
-      }, as: :json
+      assert_difference('Placement.count', 2) do
+        post api_v1_orders_url, params: @order_params, headers: {
+          Authorization: JsonWebToken.encode(user_id: @order.user_id)
+        }, as: :json
+      end
     end
     assert_response :created
   end
