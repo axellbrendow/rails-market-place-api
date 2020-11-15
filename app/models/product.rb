8fe80164260ffecf86313d846e0acaf0da8438a5
axell-brendow <breno.axel@gmail.com>
2020-11-14 23:48:55 -0300

    Add scope to sort by creation date

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,16 @@ class Product < ApplicationRecord
   scope :filter_by_title, lambda { |keyword|
     where('lower(title) LIKE ?', "%#{keyword.downcase}%")
   }
+
+  scope :above_or_equal_to_price, lambda { |price|
+    where('price >= ?', price)
+  }
+
+  scope :below_or_equal_to_price, lambda { |price|
+    where('price <= ?', price)
+  }
+
+  scope :recent, lambda {
+    order(:updated_at)
+  }
 end
